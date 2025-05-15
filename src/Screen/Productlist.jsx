62e742b5fc12c587871c5783b0a8ea964cb0aa0d
ammarify <ammarzakir321@gmail.com>
2025-05-15 15:56:06 +0500

    Fetch products from API and display list with navigation to PL Screen

--- src/Screen/Productlist.jsx
+++ src/Screen/Productlist.jsx
@@ -1,26 +1,111 @@
-import { StyleSheet, Text, View , Button} from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View , Button, FlatList, Image, ActivityIndicator, Dimensions} from 'react-native'
+import React, { useEffect, useState } from 'react'
 
 const Productlist = ({navigation}) => {
-  return (
-    <View style={styles.Container}>
-      <Text>This is a Productlist Screen</Text>
-      <Button title='GO TO PD SCREEN' onPress={()=>navigation.navigate('ProductDetail')}/>
-      <Button title='GO TO CameraScreen' onPress={()=>navigation.navigate('CameraScreen')}/>
+    const [products,setProducts] = useState([])
+    const [error , setError ] = useState(null)
+    const[loading, setLoading] = useState(true)
+
+useEffect(() => {
+    fetchProducts();
+  }, []);
+    const fetchProducts = async () => {
+        try {
+           const response = await fetch('https://fakestoreapi.com/products')
+           const data = await response.json() 
+           setProducts(data);
+        } catch (error) {
+           setError('Error fetching data')
+        }finally{
+            setLoading(false)
+        }
+    };
+    if(loading){
+        return(
+        <View style={styles.loadingContainer}>
+            <ActivityIndicator size="large" color="#000" />
+        </View>
+    );
+        
+        
+    }
+    if(error) {
+        return (
+      <Text style={styles.errorText}>
+        {error}
+      </Text>)
+    }
+ const renderItem = ({item}) => (
+    <View style={styles.card}>
+    <Image source={{uri : item.image}} style={styles.image}/>
+    <Text style={styles.title}>{item.title}</Text>
+    <Text style={styles.price}>$Price:{item.price}</Text>
     </View>
+)
+  return (
+    <FlatList 
+    data={products} renderItem={renderItem} 
+    keyExtractor={(item) => item.id.toString()}
+    numColumns={2}
+    contentContainerStyle={styles.container}
+    columnWrapperStyle={{ justifyContent: 'space-between' }}
+/>
+  
   )
 }
 
 export default Productlist
 
+const screenWidth = Dimensions.get('window').width;
+const cardWidth = (screenWidth - 48) / 2; 
+
 const styles = StyleSheet.create({
-    Container : {
-        flex : 1,
-        justifyContent : "center",
-        alignItems : "center"
+    container : {
+        padding: 16,
+        backgroundColor: '#f7f7f7'
+      
     },
-    text :  {
+    loadingContainer:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    errorText : {
+        color: 'red',
+        textAlign: 'center',
+        marginTop: 50,
+        fontSize: 18,
+    },
+    text : {
         fontSize : 20,
         marginBottom : 20
-    }
+    },
+    image: {
+        height: 120,
+        resizeMode: 'contain',
+        borderRadius: 8,
+        backgroundColor: '#f0f0f0',
+    },
+    title : {
+        fontWeight: '600',
+        fontSize: 14,
+        marginTop: 10,
+        color: '#333',
+        textAlign: 'center',
+        
+    },
+    price : {
+        marginTop: 6,
+        fontWeight: 'bold',
+        color: '#2e8b57',
+        fontSize: 16,
+        textAlign: 'center',
+    },
+    card: {
+        backgroundColor: '#fff',
+        borderRadius: 12,
+        padding: 10,
+        marginBottom: 16,
+        width: cardWidth,
+  },
 })
